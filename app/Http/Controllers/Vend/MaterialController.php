b8ef996802fa5de550cfc309c8b08a8d974de16c
zhaoyang <602155776@qq.com>
2017-04-24 16:08:21 +0800

    ..

--- app/Http/Controllers/Vend/MaterialController.php
+++ app/Http/Controllers/Vend/MaterialController.php
@@ -18,7 +18,7 @@ class MaterialController extends Controller
     //上传贩卖机领奖二维码
     public function image()
     {
-    	$qrcode = Code::where('isupload', 0)->first();
+    	$qrcode = QrCode::where('isupload', 0)->first();
     	SSQrCode::format('png')->size(250)->generate($qrcode->code, public_path('vend/'.$qrcode->code.'.png'));
     	$result = $this->material->uploadImage(public_path('vend/'.$qrcode->code.'.png'));
     	$result = json_decode($result);
