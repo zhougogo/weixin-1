c3d4a44042d1d2753dd08c370c708ed4aafc212e
zhougogo <1290612478@qq.com>
2017-05-05 13:53:44 +0800

    add
    修改弹窗

--- resources/views/hs/result.blade.php
+++ resources/views/hs/result.blade.php
@@ -14,6 +14,50 @@
                 <!--<img src="img/result.png"/>-->
             </div>
         </div>
+		<div class="page page2 hidden">
+			<div class="logo">
+				<img src="img/p2/ptBg.png" />
+			</div>
+			<div class="popup hidden errPopup">
+				<div class="content">
+					<div class="contentBg">
+						<img src="img/error/datiBg.png" />
+					</div>
+					<div class="con">
+						<img src="img/cancel.png" />
+
+					</div>
+					<p class="p1">很遗憾，正确的答案是<span>B</span></p>
+					<div class="bomBg">
+						<img src="img/win/f2.png" />
+					</div>
+					<div class="cancelBtm">
+						<img src="img/error/cancel.png" />
+					</div>
+
+				</div>
+			</div>
+			<div class="popup popup2 hidden">
+				<div class="content">
+					<div class="contentBg">
+						<img src="img/error/datiBg.png" />
+					</div>
+					<div class="con">
+						<img src="img/win/bq.png" />
+
+					</div>
+					<p class="p2">恭喜你答对了<span>4</span>题</p>
+					<p class="p3">还获得了抽奖机会</p>
+					<!--<p>很遗憾，正确的答案是<span>B</span></p>-->
+					<div class="bomBg">
+						<img src="img/beginBt m.png" />
+					</div>
+					<div class="cancelBtm">
+						<img src="img/error/cancel.png" />
+					</div>
+
+				</div>
+			</div>
 @endsection
 
 @section('script')
